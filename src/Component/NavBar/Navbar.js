d74b722783fa76b6ca105aa752e266323ccaff41
MasterBen <99284597+MasterBen25@users.noreply.github.com>
2022-08-25 17:46:07 +0200

    Create Navbar.js

---
+++ src/Component/NavBar/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Navbar.css';
+import '../../Fonts/fonts.css';
+
+function Navbar() {
+    return (
+        <ul className='quattrocento-italic nav-style'>
+            <li>Toute les variétés</li>
+            <li>Amérique du Sud</li>
+            <li>Afrique</li>
+            <li>Asie</li>
+        </ul>
+    )
+}
+
+export default Navbar;
